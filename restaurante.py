887ff0913da972607ab89c9530953a7a5a59fb77
Cannonbolt-DROID <maicol.angola@correounivalle.edu.co>
2025-04-04 22:28:16 -0500

    Update restaurante.py

--- restaurante.py
+++ restaurante.py
@@ -8,20 +8,7 @@ reserva_actual = None
 
 cancelando_reserva = False
 
-def Inicio():
-    global V1
-    V1 = tk.Tk()
-    V1.title("Restaurante Mediterráneo")
 
-    imagen = tk.PhotoImage(file="LogoRes.png")
-    label_img = tk.Label(V1, image=imagen)
-    label_img.image = imagen
-    label_img.pack(pady=20)
-
-    tk.Button(V1, text="Reservar", command=Formulario).pack(pady=10)
-    tk.Button(V1, text="Cancelar Reserva", command=CancelarReservaFormulario).pack(pady=5)
-
-    V1.mainloop()
 
 def Formulario():
     global F1, nombre, identificacion, fecha, zona, extra, frame_extra, nombre_guardado
